2ca5f6de9a7a98a980b8b76ae0a95b06b3007f07
Haldekhiel <109041734+Haldekhiel@users.noreply.github.com>
2024-08-06 06:37:56 +0300

    test

--- app/main.py
+++ app/main.py
@@ -16,8 +16,6 @@ real_estates = {
     1: RealEstate(id=1, title="Spacious Villa in Al-Malqa", price=3500000, area=380, property_type="Residential",latitude=24.6789, longitude=46.6543),
     2: RealEstate(id=2, title="Commercial Building in Olaya", price=3000000, area=520, property_type="Commercial",latitude=24.7123, longitude=46.7890),
     3: RealEstate(id=3, title="Agricultural Land in Wadi Hanifah", price=800000, area=2000, property_type="Agricultural",latitude=24.5678, longitude=46.4321),
-    4: RealEstate(id=4, title="Agricultural Land in Wadi Hanifah", price=800000, area=2000, property_type="Agricultural",latitude=24.5678, longitude=46.4321),
-
 }
 
 
