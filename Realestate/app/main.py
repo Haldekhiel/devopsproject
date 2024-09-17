ede3a02ab4f5d11ad538ca2fde730bb95198e911
Haldekhiel <109041734+Haldekhiel@users.noreply.github.com>
2024-09-17 12:38:28 +0300

    e

--- Realestate/app/main.py
+++ Realestate/app/main.py
@@ -31,7 +31,7 @@ def get_investment(name: str):
     return investment
 
 @app.post("/investments/")
-def create_investment(investment: Investment):
+def create_investment(investment: RealEstate):
     investments[investment.name] = investment
     return {"message": "Investment created successfully"}
 
